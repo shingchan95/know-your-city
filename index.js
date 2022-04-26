9a94bd650b1b2358e2e33cf86f54b6423ae10c74
Matteo Sanna <matteo.mog.sanna@gmail.com>
2022-04-26 21:36:02 +0100

    button

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const timeEl = document.getElementById('time');
-const dateEl = document
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById ('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const minutes = time.getMinutes();
+
+    timeEl.textContent = hour + ':' + minutes 
+    dateEl.textContent = days[day] + ', ' +date+ ' ' + months[month]
+
+
+}, 1000);
